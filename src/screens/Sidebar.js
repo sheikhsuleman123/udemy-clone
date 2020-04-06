394adefb09e12fe0a7eb55bb4920f1d6abd1c8ee
sheikhsuleman123 <mssheikh333@gmail.com>
2020-04-06 19:25:41 +0500

    updated course section ok

--- src/screens/Sidebar.js
+++ src/screens/Sidebar.js
@@ -4,6 +4,7 @@ import home from '../ui/home.png';
 import horn from '../ui/bullhorn.png';
 import tag from '../ui/price-tag.png';
 import Bcase from '../ui/briefcase.png';
+import Horn from '../ui/bullhorn.png';
 
 import '../css/App.css';
 
@@ -16,27 +17,35 @@ export default function Sidebar() {
         {label:'My Courses',slug:'courses',icon:Bcase},
     ])
 
+    const [currentPage,setCurrentPage] = useState("/");
+
     var navigation = [];
     for(let i =0; i < nav.length; i++){
         navigation.push(
-            <li>
-             <a href={nav[i].slug} className="link noul flex c333">
+            <li key={"nav-" + i + "-" + nav[i].slug}> 
+             <a href={nav[i].slug} className={"aic link noul flex c333" + (currentPage === nav[i].slug ? "on" : " " )}>
                <div>
                   <img  src={nav[i].icon} /> 
                 </div>
-        <h2 className="lbl s20">{nav[i].label}</h2>
+        <h2 className="lbl s15">{nav[i].label}</h2>
                 </a>
             </li>
         )
     }
     return (
-        <div className="sidebar fixed">
+        <div className="sidebar fixed ">
             <a href="#" className="logo bl">
-                <img src={logo} className="bl"/>
+                <img src={logo} className="bl ico"/>
             </a>
             <ul className="nav">
             {navigation}        
             </ul>
+            
+            <div className="updated-course flex aic">
+              <div><img src={Horn} className="ico bl s24" /> </div>
+              <div className="lbl s13 fontb c333">Updated Course</div> 
+            </div>
+              <h2 className="authore s13 c777 mr20">by Muhammad Suleman</h2>
         </div>
     )
 }
